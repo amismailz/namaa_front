d6fcbdeb104b5ebad2f02a5fd9291852abcd552b
ahmedchan <mr.ahmedsaeed1@gmail.com>
2025-09-15 13:57:55 +0300

    fix

--- components/PopularPosts.tsx
+++ components/PopularPosts.tsx
@@ -1,12 +1,14 @@
 import { BlogItemType } from "@/types.type"
 import React from "react"
 import BlogItem from "@/components/BlogItem"
+import { useLocale } from 'next-intl'
 
 const PopularPosts = ({ data }: { data: BlogItemType[] }) => {
+  const locale = useLocale()
   return (
     <div className="space-y-5">
       {data.map((item) => (
-        <BlogItem key={item.id} item={item} />
+        <BlogItem key={item.id} locale={locale} item={item} />
       ))}
     </div>
   )
