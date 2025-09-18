7f974db0b6e55b4c07937a3b74beac456ee42cce
ahmedchan <mr.ahmedsaeed1@gmail.com>
2025-09-18 13:08:06 +0300

    fix

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -3,15 +3,23 @@
 import React from "react"
 import { HTMLMotionProps, motion } from "framer-motion"
 
-const PageWrapper = (props: HTMLMotionProps<"div">) => {
+// const PageWrapper = (props: HTMLMotionProps<"div">) => {
+//   return (
+//     <div className="page-wrapper min-h-screen ">
+//       <motion.div
+//         {...props}
+//         initial={{ opacity: 0.3 }}
+//         animate={{ opacity: 1 }}
+//         transition={{ delay: 0.2 }}
+//       />
+//     </div>
+//   )
+// }
+
+const PageWrapper = ({children}: {children:React.ReactNode}) => {
   return (
     <div className="page-wrapper min-h-screen ">
-      <motion.div
-        {...props}
-        initial={{ opacity: 0.3 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.2 }}
-      />
+     {children}
     </div>
   )
 }
