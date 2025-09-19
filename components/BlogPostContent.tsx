ed138c831bcf4bb363dc8fb067bd68a192808ce8
ahmedchan <mr.ahmedsaeed1@gmail.com>
2025-09-19 18:12:15 +0300

    hot fix

--- components/BlogPostContent.tsx
+++ components/BlogPostContent.tsx
@@ -46,7 +46,7 @@ export default function BlogPostContent({
   return (
     <div
       className={cn(
-        `prose lg:prose-lg max-w-full prose-a:text-green-600 prose-a:hover:text-green-700 prose-a:underline prose-a:font-medium prose-headings:text-primary prose-img:rounded-xl prose-img:max-w-full prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5-text-lg prose-h5:font-bold prose-p:text-foreground prose-li:text-foreground `
+        `prose lg:prose-lg max-w-full prose-a:text-green-600 prose-a:hover:text-green-700 prose-a:underline prose-a:font-medium prose-headings:text-primary prose-img:rounded-xl prose-img:max-w-full prose-h1:text-4xl prose-h2:text-3xl prose-h3:text-2xl prose-h4:text-xl prose-h5-text-lg prose-h5:font-bold prose-p:text-foreground prose-li:text-foreground prose-strong:font-bold`
       )}
     >
       {parse(html, options)}
