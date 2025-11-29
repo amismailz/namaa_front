54c350330c62757dccc33f7fd36b879ab34da185
mac <mac@ahmedsaeed-2.local>
2025-11-29 21:41:21 +0200

    latest

--- components/JsonLd.tsx
+++ components/JsonLd.tsx
@@ -1,16 +1,17 @@
-
+import Script from "next/script";
 type JsonLdProps = {
   id: string
   schema: Record<string, any>
 }
 
 export function JsonLd({ id, schema }: JsonLdProps) {
+  const jsonString = JSON.stringify(schema);
   return (
-    <script
+    <Script
       id={id || "schema"}
       type="application/ld+json"
-      // strategy="afterInteractive"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      dangerouslySetInnerHTML={{ __html: jsonString }}
+      strategy="beforeInteractive"
     />
   )
 }
